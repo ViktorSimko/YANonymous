05704b228aae64bcccac9248b5fbc274b7981582
Viktor Simkó <aqviktor@gmail.com>
2016-09-14 10:52:59 +0200

    Initial Commit

---
+++ YANonymous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YANonymous
+//
+//  Created by Simkó Viktor on 2016. 09. 14..
+//  Copyright © 2016. Simkó Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
